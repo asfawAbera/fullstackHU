2736c8429aedbd122f1a91a5ae501c91e53a2078
Asfaw Abera <workin2sh@hotmail.com>
2018-11-21 15:24:33 +0200

    ex1.4 Assignment

--- src/index.js
+++ src/index.js
@@ -20,24 +20,26 @@ const Yhteensa=(props)=>{
 
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = {
-      nimi:'Reactin perusteet',
-   tehtavia : 10
-}
-  const osa2 ={
+  const osat = [
+      {
+        nimi:'Reactin perusteet',
+        tehtavia : 10
+      },
+      {
       nimi: 'Tiedonvälitys propseilla',
       tehtavia : 7
-    }
-  const osa3 ={
+    },
+    {
       nimi: 'Komponenttien tila',
      tehtavia : 14
     }
+]
 
   return (
     <div>
     <Otsikko kurssi={kurssi}/>
-    <Sisalto  osa1={osa1.nimi} tehtavia1={osa1.tehtavia} osa2={osa2.nimi} tehtavia2={osa2.tehtavia} osa3={osa3.nimi} tehtavia3={osa3.tehtavia}/>
-    <Yhteensa tehtavia1={osa1.tehtavia} tehtavia2={osa2.tehtavia} tehtavia3={osa3.tehtavia}/>
+    <Sisalto  osa1={osat[0].nimi} tehtavia1={osat[0].tehtavia} osa2={osat[1].nimi} tehtavia2={osat[1].tehtavia} osa3={osat[2].nimi} tehtavia3={osat[2].tehtavia}/>
+    <Yhteensa tehtavia1={osat[0].tehtavia} tehtavia2={osat[1].tehtavia} tehtavia3={osat[2].tehtavia}/>
   </div>)
 }
 
