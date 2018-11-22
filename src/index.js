e5e6cb5e0df88c3db4723341c0d4ed575b7e90e6
Asfaw Abera <workin2sh@hotmail.com>
2018-11-22 16:43:18 +0200

    Ex1.12 Anecdote

--- src/index.js
+++ src/index.js
@@ -1,123 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-//the first componenet of button
-const Button =(props)=>{
-return      ( 
-<table>
-<thead>
-    <tr>
-        <td><button onClick={props.good}>hyvä</button></td>
-        <td><button onClick={props.neutral}>neutraali</button></td>
-        <td><button onClick={props.bad}>huono</button></td></tr></thead>
-</table>);
-}
-//the second compononet to show the statistics
-const Stat =(props)=>{
-    return (   
-    <tbody>
-        <tr><td>hyvä</td><td> {props.good}</td></tr>
-        <tr><td>neutraali</td><td>{props.neutral}</td></tr>
-        <tr><td>huono</td><td>{props.bad}</td></tr>
-           
-  </tbody>)
-}
-//the third component to show the average and posetivity ratio
-const Stats =(props)=>{
-    return ( <tbody>
-    <tr><td>Average</td><td>{Math.round(props.ave*10)/10}</td></tr>
-    <tr><td>Posetiveity:</td><td>{props.pos}</td></tr></tbody>)
-}
-//application main holder component
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+
 class App extends React.Component {
   constructor(props){
     super(props)
     this.state={
-        hyva:0,
-        neutral:0,
-        huono:0,
-        count:0
-    }}
-good=(e)=>{
-    
-        this.setState((prevState)=>({hyva:prevState.hyva+e}))
-        this.setState((prevState)=>({count:prevState.count+1}))
-    }
-
-  
-render(){
-    const good=this.state.hyva;
-    const neutral=this.state.neutral;
-    const bad=this.state.huono;
-    const sum=good+neutral+bad;
-    const count=this.state.count;
-    const calcAverage=(good*1)+(neutral*0)+(bad*-1);
-    let ave=0;
-    ave=sum===0? ave= 0: calcAverage/sum;
-    let posRatio=Math.round((good/sum)*1000)/1000;
-    let pos=0;
-    pos=sum===0? pos="Waitin...": pos=(posRatio)*100+'%';
-    //conditional statement to return another function for the button
-    const setValue=(x)=>{
-        if(x==='good'){
-            const handler=()=>{
-                this.setState((prevState)=>({
-                    hyva:prevState.hyva+1
-                }))
-                this.setState({
-                    count:this.state.count+1
-                })
-            }
-            return handler;
-        }else if(x==='neutral'){
-            const handler=()=>{
-                this.setState((prevState)=>({
-                    neutral:prevState.neutral+1
-                }))
-                this.setState({
-                    count:this.state.count+1
-                })
-            }
-            return handler;
-
-        }else{
-            const handler=()=>{
-                this.setState((prevState)=>({
-                    huono:prevState.huono+1
-                }))
-                this.setState({
-                    count:this.state.count+1
-                })
-            }
-            return handler;
-
-        }
-    }
- 
-console.log(good)
-  return (
-<div>
-        <h1>anna palautetta</h1>
-        <Button good={setValue('good')} bad={setValue('bad')} neutral={setValue('neutral')}/>
-        <h1>Statistics</h1>
-        {
-        count!==0 
-        ? 
+        selected:0,
         
-            <table><Stat good={good} neutral={neutral} bad={bad} />
-            <Stats ave={ave} pos={pos}/></table> 
-        :
-        "ei yhtään palautetta annettu"
+    }}
+    render(){
+        const randomAnecdotes=()=>{
+            let randomNumber=Math.floor(Math.random()*anecdotes.length)+0;
+            this.setState(({selected:randomNumber}))
         }
-     
-    
+        
+        
+        return (
+            <div>
+            {this.props.anecdotes[this.state.selected]}<br />
+            <button onClick={randomAnecdotes}>Next anecdote</button>
+            </div>
+        )
+    }
 
-</div>)
-     }
 }
 
 ReactDOM.render(
-  <App />,
+  <App anecdotes={anecdotes}/>,
   document.getElementById('root')
 )
 
